f56d8b8b372e704544d7f0607c3cef744dfa53bf
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-17 03:17:03 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
   # # Return the total number of strings in the provided array using the count enumerable
-  array.count |elements| do
+  array.count do |elements|
     elements.class == String
 end
 end
